13ee3f8ec73a6c77a0146b3625464854d2aab713
jiangzhiqiang_1228 <jiangzhiqiang_1228@youzan.com>
2022-04-10 20:19:08 +0800

    feat: download template support generate package.json with user input

--- src/download/download.ts
+++ src/download/download.ts
@@ -6,17 +6,21 @@ import pkg from './template/package.json?raw'
 import config from './template/vite.config.js?raw'
 import readme from './template/README.md?raw'
 
-export async function downloadProject (store: any) {
+import { handlePackageJson } from './utils'
+import type { ReplStore } from 'src/store'
+
+export async function downloadProject (store: ReplStore) {
+  console.warn('pkg', handlePackageJson(pkg, store))
   if (!confirm('Download project files?')) {
     return
   }
 
   const { default: JSZip } = await import('jszip')
   const zip = new JSZip()
-
+  console.warn('pkg', pkg)
   // basic structure
   zip.file('index.html', index)
-  zip.file('package.json', pkg)
+  zip.file('package.json', handlePackageJson(pkg, store))
   zip.file('vite.config.js', config)
   zip.file('README.md', readme)
 
---
+++ src/download/utils.ts
@@ -0,0 +1,61 @@
+import type { ReplStore } from 'src/store'
+
+const DEFAULT_VERSION = '*'
+
+/**
+ * get version map form importMap,
+ *
+ * e.g. {
+ *
+ *  "vue": "https://cdn.jsdelivr.net/npm/@vue/runtime-dom@3.2.31/dist/runtime-dom.esm-browser.js",
+
+ *  "@vant/use": "https://cdn.jsdelivr.net/npm/@vant/use/dist/index.esm.js"
+ *
+ * }
+ *
+ * @returns { "vue": "3.2.31", "@vant/use": "*" }
+ */
+export function getPackageVersionMap (imports: Record<string, string>) {
+  return Object.entries(imports).reduce((resultImportMap, [pkgName, pkgPath]) => {
+    const version = `${pkgPath}/`.match(/@((?=\d).+?)(?=\/)/)?.[1]
+    resultImportMap[pkgName] = version ?? DEFAULT_VERSION
+    return resultImportMap
+  }, {} as Record<string, string>)
+}
+
+interface PackageJsonType {
+  'name': string,
+  'version': string,
+  'dependencies': Record<string, string>,
+  'devDependencies': Record<string, string>,
+  [key: string]: any
+}
+
+/**
+ * generate package.json with user input
+ * @param pckJsonStr package.json stringify
+ * @param store {ReplStore}
+ * @returns
+ */
+export function handlePackageJson (pckJsonStr: string, store: ReplStore) {
+  let pkgJson: Partial<PackageJsonType> = {}
+  try {
+    pkgJson = JSON.parse(pckJsonStr)
+  } catch (error) {
+    console.warn('parse package.json error:', error)
+  }
+
+  const imports = store.getImportMap()?.imports
+  if (imports) {
+    const pkgVersionMap = getPackageVersionMap(imports)
+
+    Object.entries(pkgVersionMap).forEach(([pkgName, version]) => {
+      pkgJson.dependencies = {
+        ...pkgJson.dependencies,
+        [pkgName]: version !== DEFAULT_VERSION ? version : pkgJson.dependencies?.[pkgName] ?? version
+      }
+    })
+  }
+
+  return JSON.stringify(pkgJson, null, 2)
+}
