161a24e66535ea00fc38a772576b702b219b8d6e
jiangzhiqiang_1228 <jiangzhiqiang_1228@youzan.com>
2022-04-11 13:16:18 +0800

    chore: update welcome code and  main-pic.png

--- src/config.ts
+++ src/config.ts
@@ -37,15 +37,22 @@ export const welcomeCode = `\
 <script setup lang='ts'>
 import { ref } from 'vue'
 import { Button } from 'vant';
-import { injectVant } from './${vantInjectPlugin}'
+import { injectVant } from './vant-inject-plugin.js'
 
 injectVant()
-
+const checked = ref(true);
 const msg = ref('Hello Vant!')
 </script>
 
 <template>
+  Button：
   <Button type="primary">{{ msg }}</Button>
+  <van-divider />
+  van-button：
+  <van-button type="primary">{{ msg }}</van-button>
+  <van-divider />
+  van-Button：
+  <van-Button type="primary">{{ msg }}</van-Button>
 </template>
 `
 
