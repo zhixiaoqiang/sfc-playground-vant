089459cc4ed6eff813f23946e4f9a715cd9ad804
jiangzhiqiang_1228 <jiangzhiqiang_1228@youzan.com>
2022-04-14 14:13:38 +0800

    chore(scripts): add some comment

--- scripts/utils/enhanceArgv.mjs
+++ scripts/utils/enhanceArgv.mjs
@@ -1,7 +1,13 @@
 import { argv } from 'zx'
 
+/**
+ * value === 'false' || value === false
+ */
 const isFalse = (value) => value === 'false' || value === false
 
+/**
+ * if argv value isFalse convert to false
+ */
 const Argv = Object.fromEntries(Object.entries(argv).map(([key, value]) => [key, isFalse(value) ? false : value]))
 
 export {
