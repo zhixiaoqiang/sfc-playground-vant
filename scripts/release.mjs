4fd3ea2a93c68a62f498dfc6b0913ec01e368652
jiangzhiqiang_1228 <jiangzhiqiang_1228@youzan.com>
2022-04-15 21:07:20 +0800

    chore(scripts): release.mjs add repo push

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -9,17 +9,25 @@ const json = JSON.parse((String(packageJson)))
 /**
  * --tagPublish set tagPublish to true, default true
  * --no-tagPublish set tagPublish to false or --tagPublish=false
+ * --pushRepo set pushRepo to true, default true
+ * --no-pushRepo set pushRepo to false or --pushRepo=false
  * --npmPublish set npmPublish to true, default false
  * --no-npmPublish set npmPublish to false or --npmPublish=false
  *
- * NOTE: pnpm args: pnpm xx -- --tagPublish --npmPublish
+ * NOTE: pnpm args: pnpm xx -- --tagPublish --pushRepo --npmPublish
  */
-const { tagPublish = true, npmPublish } = Argv
+const { tagPublish = true, pushRepo = true, npmPublish } = Argv
 
 if (tagPublish) {
   await $`git tag v${json.version}`
   await $`git push origin --tags`
 }
+
+if (pushRepo) {
+  const currentBranch = await $`git branch --show-current`
+  await $`git push origin ${currentBranch}`
+}
+
 if (npmPublish) {
   await $`pnpm publish --access public`
 }
